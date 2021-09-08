2a539662df15492acd5c0a878207d018c458b391
soobinism <kibinsoo@gmail.com>
2021-09-08 18:22:54 -0400

    Added a button to app centered under text greeting using the activity_main.xml design window. The text in the button changes on click to 'You have clicked the button.' Added this line of code in MainActivity.kt.

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,11 +2,17 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById<Button>(R.id.button)
+            button.setOnClickListener({button.text = "You clicked the button"})
     }
 }
+
+
